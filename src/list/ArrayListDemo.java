c63e28a13d3ac1129ca1bd41fdfdbcde35b65b95
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2021-03-31 20:12:01 +0700

    Initial Commit :3

---
+++ src/list/ArrayListDemo.java
@@ -0,0 +1,71 @@
+package list;
+
+import java.util.*;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+//        List Iterator method
+        System.out.println("-----------List Iterator method-----------");
+
+        List<Integer> list = new ArrayList<>();
+        for (int idx = 0; idx < 10; idx++) {
+            list.add(idx);
+        }
+
+        ListIterator<Integer> listIterator = list.listIterator();
+        System.out.print("Before modification: ");
+        while(listIterator.hasNext()) {
+            System.out.print(listIterator.next() + " ");
+        }
+        System.out.println();
+
+        listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            int item = listIterator.next();
+            if (item % 2 == 0) {
+                listIterator.set(item * 10);
+            } else {
+                listIterator.remove();
+            }
+        }
+
+        listIterator = list.listIterator();
+        System.out.print("After modification: ");
+        while (listIterator.hasNext()) {
+            System.out.print(listIterator.next() + " ");
+        }
+        System.out.println();
+
+//        List Index method
+        System.out.println("------------List Index method-------------");
+
+        list.clear();
+        for (int idx = 0; idx < 10; idx++) {
+            list.add(idx);
+        }
+
+        listIterator = list.listIterator();
+        System.out.print("Before modification: ");
+        while(listIterator.hasNext()) {
+            System.out.print(listIterator.next() + " ");
+        }
+        System.out.println();
+
+        for (int idx = 0; idx < list.size(); idx++) {
+            int item = list.get(idx);
+            if (item % 2 == 0) {
+                list.set(idx, item * 10);
+            } else {
+                list.remove(idx);
+                idx -= 1;
+            }
+        }
+
+        listIterator = list.listIterator();
+        System.out.print("After modification: ");
+        while (listIterator.hasNext()) {
+            System.out.print(listIterator.next() + " ");
+        }
+        System.out.println();
+    }
+}
